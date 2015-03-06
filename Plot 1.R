6dbc0c014a8c795507f59a26c05b175c73ef3d21
Carlos Jesus Fernandez Basso <cjferba@gmail.com>
2015-03-06 12:16:06 +0100

    end plot1

--- Plot 1.R
+++ Plot 1.R
@@ -1,9 +1,8 @@
 
 source("Load_Data.R")
 
+png(filename = "Plot 1.png",width = 480,height = 480,bg = "transparent"  )
 
 hist(DataSet$Global_active_power,col="red",xlab = "Global active power (kilowatts)",main="Global active power")
 
-
-
-
+dev.off()
